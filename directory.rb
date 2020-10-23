001a406c0838b47bda1ee95f58020a04945d88c5
CodeZeus-dev <constantine.akritides@gmail.com>
2020-10-23 16:41:01 +0100

    Added shebang atop the directory.rb script to run it using ./directory.rb, and removed comments from top

--- directory.rb
+++ directory.rb
@@ -1,17 +1,4 @@
-# # Putting all student names into an array calles students
-# students = [
-#   {name: "Dr. Hannibal Lecter", cohort: :november},
-# {name: "Darth Vader", cohort: :november},
-# {name: "Nurse Ratched", cohort: :november},
-# {name: "Michael Corleone", cohort: :november},
-# {name: "Alex DeLarge", cohort: :november},
-# {name: "The Wicked Witch of the West", cohort: :november},
-# {name: "Terminator", cohort: :november},
-# {name: "Freddy Krueger", cohort: :november},
-# {name: "The Joker", cohort: :november},
-# {name: "Joffrey Baratheon", cohort: :november},
-# {name: "Norman Bates", cohort: :november}
-# ]
+#!/usr/bin/env ruby
 require 'date'
 
 class StudentDirectory
